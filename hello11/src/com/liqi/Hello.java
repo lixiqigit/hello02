7c07f8cb959674dd68c9a863cc98f4966da4e7bf
liqi <1363676627@qq.com>
2019-07-09 18:13:12 +0800

    添加22222

--- hello11/src/com/liqi/Hello.java
+++ hello11/src/com/liqi/Hello.java
@@ -8,4 +8,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("dssfsdf");
     }
+    public void test(){
+        System.out.println("这是一个test");
+    }
 }
